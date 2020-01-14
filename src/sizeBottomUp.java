6a81d65556cdc2ecc44513c4cd681349e71e8aa1
shaharewq0 <cohenhas@bgu.post.ac.il>
2020-01-14 12:22:16 +0200

    SizeBottomUp&projectCreation

---
+++ src/sizeBottomUp.java
@@ -0,0 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class sizeBottomUp {
+    sizeBottomUp
+    @Before
+    public void setUp() throws Exception {
+        arr= new int[]{1, 2, 3};
+    }
+
+    @Test(timeout = 100)
+    public void size() {
+        assertEquals(3,Program.size(arr));
+        assertNotEquals(0,Program.size(arr));
+    }
+}
