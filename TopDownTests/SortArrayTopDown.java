06172b3f03c36789db07ef7a31c90c2c6bfa2aa2
OmerSudri <37580280+OmerSudri@users.noreply.github.com>
2020-01-19 15:50:51 +0200

    update tests

--- TopDownTests/SortArrayTopDown.java
+++ TopDownTests/SortArrayTopDown.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 /*
-    Test #7
+    Test #8
     Test: isSorted, merge, printSorted
     Real Functions: printArr, equalArrays, sortArray
     Stubs: size, copyArr
@@ -34,27 +34,19 @@ public class SortArrayTopDown {
                 //isSorted parameters
                 {TestType.isSorted, new int[]{1, 2, 3, 4}, null, true},
                 {TestType.isSorted, new int[]{3, 2, 4, 1}, null, false},
-                {TestType.isSorted, new int[]{4, 3, 2, 1}, null, false},
-                {TestType.isSorted, new int[]{1, 2, 1, 2}, null, false},
                 {TestType.isSorted, new int[]{}, null, true},
-                {TestType.isSorted, new int[]{1}, null, true},
 
                 //merge parameters
                 {TestType.merge, new int[]{1, 2}, new int[]{3, 4}, new int[]{1, 2, 3, 4}},
-                {TestType.merge, new int[]{3, 2}, new int[]{4, 1}, new int[]{1, 2, 3, 4}},
                 {TestType.merge, new int[]{4, 3, 2}, new int[]{1}, new int[]{1, 2, 3, 4}},
                 {TestType.merge, new int[]{3, 2, 4, 1}, null, new int[]{1, 2, 3, 4}},
                 {TestType.merge, null, new int[]{3, 2, 4, 1}, new int[]{1, 2, 3, 4}},
-                {TestType.merge, new int[]{}, new int[]{}, new int[]{}},
                 {TestType.merge, null, null, null},
 
                 //printSorted parameters
                 {TestType.printSorted, new int[]{1, 2, 3, 4}, null, "1 2 3 4 \r\n1 2 3 4 \r\n1 2 3 4 \r\n"},
                 {TestType.printSorted, new int[]{3, 2, 4, 1}, null, "3 2 4 1 \r\n1 2 3 4 \r\n3 2 4 1 \r\n"},
-                {TestType.printSorted, new int[]{4, 3, 2, 1}, null, "4 3 2 1 \r\n1 2 3 4 \r\n4 3 2 1 \r\n"},
-                {TestType.printSorted, new int[]{1, 2, 1, 2}, null, "1 2 1 2 \r\n1 1 2 2 \r\n1 2 1 2 \r\n"},
                 {TestType.printSorted, new int[]{}, null, "\r\n\r\n\r\n"},
-                {TestType.printSorted, new int[]{1}, null, "1 \r\n1 \r\n1 \r\n"},
                 {TestType.printSorted, null, null, "No array\r\n"}
         });
     }
